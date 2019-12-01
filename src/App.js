c40ee72eba821c3d590c17d66807c8cf5ca4898b
ALFM126 <55653062+ALFM126@users.noreply.github.com>
2019-12-01 12:19:02 +0000

    API integration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
@@ -19,5 +19,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="row">
-        <div className="col-sm-6">
-          <form>
-            <input
-              type="search"
-              placeholder="Enter City..."
-              autocomplete="off"
-              autoFocus="on"
-            ></input>
-            <input type="submit" value="Search"></input>
-          </form>
-        </div>
-        <div className="col-sm-6">
-          <h1>Braga</h1>
-        </div>
-      </div>
-      <p>Sunday, November 10, 2019</p>
-      <p>22:00</p>
-      <div className="row">
-        <div className="col-sm-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
-              alt="description"
-              className="float-left"
-            ></img>
-            <div className="float-left">
-              <span>6</span>
-              <a href="/">ºC</a>
-            </div>
+  const [ready, setReady] = useState(false);
+  const [data, setData] = useState({});
+  function handleResponse(response) {
+    setData({
+      time: "Sunday, December 1, 2019",
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/rain.png",
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity
+    });
+
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <div className="row">
+          <div className="col-sm-7">
+            <form>
+              <input
+                type="search"
+                placeholder="Enter City..."
+                autocomplete="off"
+                autoFocus="on"
+              ></input>
+              <input type="submit" value="Search"></input>
+            </form>
+          </div>
+          <div className="col-sm-5">
+            <h1>{data.city}</h1>
           </div>
         </div>
-        <div className="col-sm-6">
-          <ul>
-            <li>Rain</li>
-            <li>Humidity: 66%</li>
-            <li>Wind Speed: 11km/h</li>
-          </ul>
+        <p>{data.time}</p>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="clearfix">
+              <img
+                src={data.iconUrl}
+                alt={data.description}
+                className="float-left"
+              ></img>
+              <div className="float-left">
+                <span className="temperature">
+                  {Math.round(data.temperature)}
+                </span>
+                <a href="/">ºC</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <ul>
+              <li className="text-capitalize">{data.description}</li>
+              <li>Humidity: {data.humidity}%</li>
+              <li>Wind Speed: {Math.round(data.wind)}km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "8c52e04f20e2246bd2fa1beb9972163c";
+    let city = "Braga";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
